b87c9470cddfaaef6bcbc0f64fd6fd880006a4fa
SebiNechita <A.S.Nechita-1@student.tudelft.nl>
2026-01-30 18:50:00 +0100

    feature: created the datasets folder and added the 3 CSVs created + created the plots for Health Care, Elections, Economy, Taxes, Immigration, Education, Crime and Jobs

--- clean-dataset.py
+++ clean-dataset.py
@@ -3,10 +3,10 @@
 
 import pandas as pd
 # Load the dataset
-data = pd.read_csv('politifact.csv')
+data = pd.read_csv('datasets/politifact.csv')
 # Filter out rows that contain 'PolitiFact en Español' in the 'tags' column
 cleaned_data = data[~data['tags'].str.contains('PolitiFact en Español', na=False)]
 # Filter out rows whose verdict is 'np-flip','half-flip','full-flop'
 cleaned_data = cleaned_data[~cleaned_data['verdict'].isin(['np-flip', 'half-flip', 'full-flop'])]
 # Save the cleaned dataset to a new CSV file    
-cleaned_data.to_csv('politifact-english.csv', index=False)
+cleaned_data.to_csv('datasets/politifact-english.csv', index=False)
